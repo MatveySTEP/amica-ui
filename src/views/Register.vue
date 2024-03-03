e760c405aab0fb72fad5d0cab4d6023afb650775
MatveySTEP <matrasser@mail.ru>
2024-03-03 14:48:46 +0400

    Initial commit

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -23,7 +23,7 @@ export default {
       password: ''
     })
     const submit = async () => {
-      await ('http://127.0.0.1:8100/v1/api/register', {
+      await fetch ('http://127.0.0.1:8100/v1/api/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
