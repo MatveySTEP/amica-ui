440cc2b64c50543790e9927a39293133cdfeb28b
Sireax <sireax@yandex.ru>
2024-03-17 23:31:11 +0400

    local deploy, cors fixed

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,26 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// import axios from 'axios'
+import axios from 'axios'
 
-// axios.defaults.withCredentials = true // Include cookies in requests
+axios.defaults.withCredentials = true // Include cookies in requests
 
-// axios.interceptors.response.use(
-//   response => {
-//     // Check if the response contains a 'Set-Cookie' header and handle the cookie as needed
-//     const cookie = response.headers['set-cookie']
-//     if (cookie) {
-//       // Handle the cookie here (e.g., save it in localStorage)
-//       console.log('Received cookie:', cookie)
-//     }
-//     return response
-//   },
-//   error => {
-//     return Promise.reject(error)
-//   }
-// )
+axios.interceptors.response.use(
+  response => {
+    // Check if the response contains a 'Set-Cookie' header and handle the cookie as needed
+    const cookie = response.headers['set-cookie']
+    if (cookie) {
+      // Handle the cookie here (e.g., save it in localStorage)
+      console.log('Received cookie:', cookie)
+    }
+    return response
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.mount('#app')
